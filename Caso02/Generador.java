b6141c11433cb57817260b18ff91dc03a6c1e1d3
isaz111 <i.salcedod@uniandes.edu.co>
2026-03-07 16:49:20 -0500

    .

--- Caso02/Generador.java
+++ Caso02/Generador.java
@@ -1,9 +1,9 @@
-public class Generador{ 
-    public static void main(Integer[] Nf1, Integer[] Nc1, Integer[] Nf2, Integer[] Nc2,
-        Integer[] tPag, String[] nomArchivo) {
-      
-        
+public class Generador {
+
+    public static void main(String[] args) {
+        if (args.length != 6) {
+            System.out.println("Uso: java Generador NF1 NC1 NF2 NC2 TP archivoSalida");
+            return;
+        }
     }
-    
-    
 }
