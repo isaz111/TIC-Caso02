f7363053cd58195bfa575b506b7d66395ed7fbb5
Isa Salcedo <i.salcedod@uniandes.edu.co>
2026-03-07 15:39:53 -0500

    Add files via upload

---
+++ Generador.java
@@ -0,0 +1,9 @@
+public class Generador{ 
+    public static void main(Integer[] Nf1, Integer[] Nc1, Integer[] Nf2, Integer[] Nc2,
+        Integer[] tPag, String[] nomArchivo) {
+      
+        
+    }
+    
+    
+}
